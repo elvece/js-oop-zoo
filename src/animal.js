e248c38a43a94fc254a5156abba56b079d9d0243
elvece <lvcifferello@gmail.com>
2015-08-12 13:28:12 -0600

    added comments to all functions, classes, and methods

--- src/animal.js
+++ src/animal.js
@@ -1,10 +1,13 @@
+//animal class constructor, takes name, age and kind, has default setting of awake as false (ie animal starts as asleep)
 function Animal(name, age, kind){
   this.name = name;
   this.age = age;
   this.kind = kind;
+  //default awake is false
   this.awake = false;
 }
 
+//method of animal class that returns a string if kind is a pig
 Animal.prototype.oink = function(){
   if (this.kind === 'Pig'){
     return 'oink';
@@ -13,15 +16,18 @@ Animal.prototype.oink = function(){
   }
 };
 
+//method of animal class that adds one to the age of animal
 Animal.prototype.growUp = function(){
   return this.age += 1;
 };
 
+//method of animal class that sets the awake value to true, thus waking up the animal
 Animal.prototype.wakeUp = function(){
   this.awake = true;
   return this.awake;
 };
 
+//method of animal class that feeds the animal if it is awake
 Animal.prototype.feed = function(){
   if (this.awake === true){
     return "NOM NOM NOM";
@@ -31,6 +37,7 @@ Animal.prototype.feed = function(){
   }
 };
 
+//method of animal class that puts the animal to sleep if it is awake
 Animal.prototype.sleep = function(){
   if (this.awake === true){
     this.awake = false;
@@ -38,6 +45,5 @@ Animal.prototype.sleep = function(){
   return this.awake;
 };
 
-
-
+//testing export
 module.exports = Animal;
--- src/zoo.js
+++ src/zoo.js
@@ -1,5 +1,7 @@
+//requires data from animal file
 var Animal = require("./animal");
 
+//zoo class constructor, takes name and location and has default setting of closes and empty array of animals
 function Zoo(name, location){
   this.name = name;
   this.location = location;
@@ -7,21 +9,25 @@ function Zoo(name, location){
   this.animals = [];
 }
 
+//method of zoo class that changes the zoo location
 Zoo.prototype.changeLocation = function(newLocation) {
   this.location = newLocation;
   return this.location;
 };
 
+//method of zoo class that changes the status of the zoo to open
 Zoo.prototype.open = function() {
   this.status = 'Open!';
   return this.status;
 };
 
+//method of zoo class that changes the status of the zoo to closed
 Zoo.prototype.close = function() {
   this.status = 'Closed!';
   return this.status;
 };
 
+//method of zoo class that checks to see if the zoo is open, otherwise it returns the closed status
 Zoo.prototype.isOpen = function() {
   if (this.status == 'Open!'){
     return this.status;
@@ -31,6 +37,7 @@ Zoo.prototype.isOpen = function() {
   }
 };
 
+//method of zoo class that adds the animal to the zoo animal array if the zoo is open, if the animal is an instance of the Animal class, and if the animal is not already in the zoo animal array; returns error messages if zoo is closed or animal is not a valid animal, otherwise returns the last animal in the array
 Zoo.prototype.addAnimal = function(animal) {
   if (this.status == 'Open!'){
     if (animal instanceof Animal){
@@ -51,6 +58,7 @@ Zoo.prototype.addAnimal = function(animal) {
   }
 };
 
+//method of the zoo class that removes the animal from the zoo animal array if the animal is in the animals array, returns the zoo animal array
 Zoo.prototype.removeAnimal = function(animal) {
   var index = this.animals.indexOf(animal);
   if(index >= 0){
